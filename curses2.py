4329b14f9c5a5e81fec2d758ee8c01c7052258f6
Josh Holm <joshwh21@gmail.com>
2026-03-23 15:05:07 -0400

    first working user input term

---
+++ curses2.py
@@ -0,0 +1,28 @@
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+
+    begin_x = 20; begin_y = 7
+    height = 5; width = 40
+    win = curses.newwin(height, width, begin_y, begin_x)
+    
+    stdscr.addstr("Press 'p', 'UP arrow', or 'q' to quit.\n")
+    stdscr.refresh()
+
+    while True:
+        c = stdscr.getch()
+        
+        if c == ord('p'):
+            stdscr.addstr("\nYou pressed p")
+        elif c == ord('q'):
+            break
+        elif c == curses.KEY_UP:
+            stdscr.addstr("\nYou pressed the UP key")
+        
+        stdscr.refresh()
+
+# Run
+if __name__ == "__main__":
+    wrapper(main)
